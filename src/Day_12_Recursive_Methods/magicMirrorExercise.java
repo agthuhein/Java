d9235611db71bfb22ad10cbbf3e29f53c9823670
Aung Thu Hein <agthuhein.mm@gmail.com>
2025-02-25 17:34:47 +0100

    newly add

--- src/Day_12_Recursive_Methods/magicMirrorExercise.java
+++ src/Day_12_Recursive_Methods/magicMirrorExercise.java
@@ -1,4 +1,22 @@
 package src.Day_12_Recursive_Methods;
 
+import java.util.Scanner;
+
 public class magicMirrorExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string to reflect in the magic mirror: ");
+        String input = scanner.nextLine();
+        String reverseString = reverseSting(input);
+        System.out.println("The magic mirror reflect: " + reverseString);
+        scanner.close();
+    }
+    public static String reverseSting(String str) {
+        //Base case
+        if(str.isEmpty() || str.length() == 1){
+            return str;
+        }
+        //recursive case
+        return str.charAt(str.length()-1) + reverseSting(str.substring(0, str.length()-1));
+    }
 }
--- src/Day_12_Recursive_Methods/methodCallStack.java
+++ src/Day_12_Recursive_Methods/methodCallStack.java
@@ -1,4 +1,30 @@
 package src.Day_12_Recursive_Methods;
 
 public class methodCallStack {
+    public static void main(String[] args) {
+        makeSandwich();
+    }
+    public static void getSlicesOfBread(){
+        buyBread();
+        System.out.println("Got two slices of bread");
+    }
+    public static void buyBread(){
+        System.out.println("Bought two slices of bread");
+    }
+    public static void spreadPeanutButter(){
+        System.out.println("Spread Peanut Butter on one slice.");
+    }
+    public static void spreadJelly(){
+        System.out.println("Spread Jelly on other slice.");
+    }
+    public static void putSlicesTogether(){
+        System.out.println("Put Slices");
+    }
+    public static void makeSandwich(){
+        getSlicesOfBread();
+        spreadPeanutButter();
+        spreadJelly();
+        putSlicesTogether();
+        System.out.println("Sandwich is ready!");
+    }
 }
