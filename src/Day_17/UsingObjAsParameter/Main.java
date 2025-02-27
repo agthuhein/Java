34810e8d9976b31e500101b9209fb3e78ca3eedc
Aung Thu Hein <agthuhein.mm@gmail.com>
2025-02-27 10:49:57 +0100

    added

--- src/Day_17/UsingObjAsParameter/Main.java
+++ src/Day_17/UsingObjAsParameter/Main.java
@@ -2,6 +2,14 @@ package src.Day_17.UsingObjAsParameter;
 
 public class Main {
     public static void main(String[] args) {
+        Smartphone sp1 = new Smartphone("iPhone 15", "Apple", 1200);
+        Smartphone sp2 = new Smartphone("A21", "Samsung", 1100);
 
+        sp1.applyDiscount(20);
+        sp1.comparePrice(sp2);
+        sp2.comparePrice(sp1);
+
+        sp1.displayDetails();
+        sp2.displayDetails();
     }
 }
